b73d74eef0f828a8ab902f6017fef8403fdb792e
noa207 <noa9090@gmail.com>
2023-06-05 09:05:34 +0300

    Add `randomfact` command

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,5 +1,5 @@
 
-
+import json
 import random
 
 import aiohttp
@@ -113,6 +113,30 @@ class Fun(commands.Cog, name="fun"):
                 if request.status == 200:
                     data = await request.json()
                     embed = discord.Embed(description=data["text"], color=0xD75BF4)
+                else:
+                    embed = discord.Embed(
+                        title="Error!",
+                        description="There is something wrong with the API, please try again later",
+                        color=0xE02B2B,
+                    )
+                await context.send(embed=embed)
+
+    @commands.hybrid_command(name="randomadvice", description="Get a random advice.")
+    @checks.not_blacklisted()
+    async def randomadvice(self, context: Context) -> None:
+        """
+        Get a random advice.
+
+        :param context: The hybrid command context.
+        """
+        # This will prevent your bot from stopping everything when doing a web request - see: https://discordpy.readthedocs.io/en/stable/faq.html#how-do-i-make-a-web-request
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                "https://api.adviceslip.com/advice"
+            ) as request:
+                if request.status == 200:
+                    data = json.loads(await request.text())
+                    embed = discord.Embed(description=data["slip"]["advice"], color=0xD75BF4)
                 else:
                     embed = discord.Embed(
                         title="Error!",
